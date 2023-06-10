6ec27cfcf7ab9e93a900fbfaf8c5146d46f5b569
DmytroKhmaruk <dima.khmaruk7@gmail.com>
2023-06-10 19:49:34 +0200

    зробив ДЗ

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const imgHtml = galleryItems.map(img => {
+  return `<li class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+        data-source="${img.original}"
+        alt="${img.description}"
+      />
+    </a>
+  </li>`;
+}).join('');
+
+gallery.insertAdjacentHTML('afterbegin', imgHtml);
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const image = event.target;
+  openModal(image.dataset.source);
+}
+
+function openModal(src) {
+  const modal = basicLightbox.create(`
+    <img
+      src="${src}"
+    />
+  `);
+
+  modal.show();
+  document.addEventListener('keyup', onCloseModalByKey);
+  modal.element().addEventListener('click', onCloseModalByClick);
+
+  function onCloseModalByKey(event) {
+    if (event.code === 'Escape') {
+      modal.close();
+      document.removeEventListener('keyup', onCloseModalByKey);
+    }
+  }
+
+  function onCloseModalByClick(event) {
+    if (event.target === modal.element()) {
+      modal.close();
+      modal.element().removeEventListener('click', onCloseModalByClick);
+    }
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+const imgHtml = galleryItems.map(img => {
+  return `<li class="gallery__item">
+        <a class="gallery__link" href="${img.original}">
+          <img
+            class="gallery__image"
+            src="${img.preview}"
+            alt="${img.description}"
+          />
+        </a>
+      </li>`;
+}).join('');
+
+gallery.insertAdjacentHTML('afterbegin', imgHtml);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+      captionsPosition: 'bottom',
+    });
+
 console.log(galleryItems);
